68819467528e0cb2d5011f9b93bac1b7ab11af65
Jesse <illmadecoder@gmail.com>
2022-12-10 02:10:34 -0500

    prompt tree and formatting

--- scripts/game_scripts/init_day.sh
+++ scripts/game_scripts/init_day.sh
@@ -12,9 +12,6 @@ then
     exit 1
 fi
 
-# day jq
-# pokedex_start=$(jq '.import.pokedex.start' $days_path)
-# pokedex_end=$(jq '.import.pokedex.end' $days_path)
 day_data_path=$days_data_path/$1.yaml
 pokedex_ids=$(yq '.import.pokedex.ids' $day_data_path)
 pokedex_version=$(yq '.import.pokedex.version' $day_data_path)
@@ -23,21 +20,7 @@ pokedex_data_path="$pokedex_data_path/$pokedex_version"
 for id in $pokedex_ids
 do
     pokedex_data_member="$pokedex_data_path/$id.yaml"
-    echo $pokedex_data_member
-    echo "$import_path/$id.yaml"
-    cp $pokedex_data_member "$import_path/$id.yaml"
+    #todo: remove yq dependency
+    name=$(yq .name $pokedex_data_member)
+    cp $pokedex_data_member "$import_path/$name"
 done
-
-# pokemon
-# pokemon_ids=$(seq $pokedex_start $pokedex_end)
-
-# for i in $pokemon_ids
-# do
-#     pokemon=$pokemon_cache_path/$i
-#     echo $pokemon
-#     pokemon_name=$(jq -r ".name" $pokemon)
-#     pokemon_id=$(jq ".id" $pokemon)
-#     pokemon_id_name="${pokemon_id}_${pokemon_name}"
-#     pokemon_id_name
-    
-# done
